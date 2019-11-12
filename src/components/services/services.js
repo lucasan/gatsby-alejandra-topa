7634c34e1be50a5ff3cabfad77dc0ca6b4203b4c
Lucas Torres <lucas@agileana.com>
2019-11-11 20:34:21 -0500

    Re order services

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import { Container, Row, Col } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 import { Link } from "../link-animations"
 import Image from "../image"
 import "./assets/scss/styles.scss"
@@ -8,30 +8,30 @@ import "./assets/scss/styles.scss"
 const ServicesQuery = () => {
   const services = useStaticQuery(
     graphql`
-      query {
-        allWordpressPage(filter: {parent_element: {template: {eq: "template-services.php"}}}) {
-          edges {
-            node {
-              title
-              path
-              acf {
-                resumen
-                imagen_en_resumen {
-                  alt_text
-                  localFile {
-                    childImageSharp {
-                      fluid(maxWidth: 400) {
-                        srcSetWebp
-                        srcSet
-                      }
+        query {
+            allWordpressPage(filter: {parent_element: {template: {eq: "template-services.php"}}}, sort: {fields: menu_order, order: ASC}) {
+                edges {
+                    node {
+                        title
+                        path
+                        acf {
+                            resumen
+                            imagen_en_resumen {
+                                alt_text
+                                localFile {
+                                    childImageSharp {
+                                        fluid(maxWidth: 400) {
+                                            srcSetWebp
+                                            srcSet
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                  }
                 }
-              }
             }
-          }
         }
-      }
     `,
   )
 
@@ -46,13 +46,13 @@ const Services = (props) => {
 
     return (
       <Col key={index} xs={12} md={4}>
-        <div className={'service-image'}>
-          <Image image={service.node.acf.imagen_en_resumen} className={'img'}/>
+        <div className={"service-image"}>
+          <Image image={service.node.acf.imagen_en_resumen} className={"img"}/>
         </div>
         <h3>{service.node.title}</h3>
-        <article dangerouslySetInnerHTML={{__html: service.node.acf.resumen}} />
-        <div className={'btn-holder'}>
-          <Link className={'btn btn-info'} to={service.node.path}>
+        <article dangerouslySetInnerHTML={{ __html: service.node.acf.resumen }}/>
+        <div className={"btn-holder"}>
+          <Link className={"btn btn-info"} to={service.node.path}>
             Más información
           </Link>
         </div>
@@ -61,10 +61,10 @@ const Services = (props) => {
   })
 
   return (
-    <section id={'services'}>
+    <section id={"services"}>
       <Container>
         <Row>
-          <Col className={'section-main-title'}><h2>Servicios</h2></Col>
+          <Col className={"section-main-title"}><h2>Servicios</h2></Col>
         </Row>
         <Row>
           {rows}
