14214c0144ceabc666fb13b99c4dfcdbd78a17a7
Lucas Torres <lucas@agileana.com>
2019-11-13 16:55:41 -0500

    Adjust Blog listing page

--- src/templates/listing.js
+++ src/templates/listing.js
@@ -25,7 +25,7 @@ export default (props) => {
       <Container>
         <article dangerouslySetInnerHTML={{ __html: page.content }}/>
       </Container>
-      <Container>
+      <Container id={'blog'}>
         <Row>
           <ListingItem posts={posts}/>
         </Row>
