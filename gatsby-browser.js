58687ad78a7c0ff3bbc2dafceacf2472db56ba9f
Lucas Torres <lucas@agileana.com>
2019-11-13 17:09:18 -0500

    Adjust browser issues with Gastby

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,3 +12,23 @@ exports.onRouteUpdate = (location) => {
     }, transitionDelay)
   }
 }
+
+// Push a new event to the Google Tag Manager datalayer
+exports.onRouteUpdate = function (_, pluginOptions) {
+  if (process.env.NODE_ENV === "production" || pluginOptions.includeInDevelopment) {
+    // wrap inside a timeout to ensure the title has properly been changed
+    setTimeout(function () {
+      window.dataLayer.push({
+        event: "gatsby-route-change"
+      });
+    }, 50);
+  }
+};
+
+// Fix the Gatsby issue with the manual browser navigation (back and forward buttons)
+// a bit hacky but works
+exports.onInitialClientRender = () => {
+  window.addEventListener('popstate', () =>
+    window.location.href = window.location.href
+  )
+}
