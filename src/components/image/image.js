c82db412572657d85b9894ae0e7f7836b52fe020
Lucas Torres <lucas@agileana.com>
2019-11-24 11:17:34 -0500

    Load image conditionally

--- src/components/image/image.js
+++ src/components/image/image.js
@@ -3,13 +3,18 @@ import React from "react"
 const Image = (props) => {
   const image = props.image
   const className = props.className
-  return (
-    <picture>
-      <source srcSet={image.localFile.childImageSharp.fluid.srcSetWebp} type={'image/webp'} />
-      <img src={image.localFile.childImageSharp.fluid.originalImg} srcSet={image.localFile.childImageSharp.fluid.srcSet}
-           alt={image.alt_text} className={className}/>
-    </picture>
-  )
+  if (image) {
+    return (
+      <picture>
+        <source srcSet={image.localFile.childImageSharp.fluid.srcSetWebp} type={"image/webp"}/>
+        <img src={image.localFile.childImageSharp.fluid.originalImg}
+             srcSet={image.localFile.childImageSharp.fluid.srcSet}
+             alt={image.alt_text} className={className}/>
+      </picture>
+    )
+  } else {
+    return (<></>)
+  }
 }
 
 export default Image
